12ab1ac76ce4fe83c66f5a44330dfc694c6c537f
Mahmoud Khashaba <mahmoud.h.khashaba@gmail.com>
2025-08-20 19:57:34 +0300

    fixes

--- pages/sign-in/[[...index]].jsx
+++ pages/sign-in/[[...index]].jsx
@@ -1,9 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
-const SignInPage = () => (
-  <div className="relative min-h-screen sm:flex sm:items-center sm:justify-center">
-    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-  </div>
-)
-
-export default SignInPage
+export default function Page() {
+  return (
+    <div className="relative min-h-screen sm:flex sm:items-center sm:justify-center">
+      <SignIn />
+    </div>
+  )
+}
